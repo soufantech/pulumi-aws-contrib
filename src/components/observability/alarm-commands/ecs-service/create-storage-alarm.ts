a407ce6cb7a24b013bac681edab0f689cc59e1a1
josemorista <josemorista@id.uff.br>
2022-08-03 17:29:40 -0300

    refactor: refactored netowrk and storage ecs alarms

---
+++ src/components/observability/alarm-commands/ecs-service/create-storage-alarm.ts
@@ -0,0 +1,57 @@
+import AlarmBuilder from '../../builders/alarm-builder';
+import { CreateAlarmCommand } from '../../commands/create-alarm-command';
+import AlarmStore from '../../resources/alarm-store';
+import { AlarmExtraConfigs, EcsServiceConfig } from '../../types';
+
+export default class CreateStorageAlarmCommand implements CreateAlarmCommand {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(
+        readonly name: string,
+        readonly threshold: number,
+        readonly configs: EcsServiceConfig,
+        readonly extraConfigs: AlarmExtraConfigs = {},
+        readonly input: 'write' | 'read'
+    ) { }
+
+    execute(parent?: AlarmStore) {
+        const { clusterName, serviceName } = this.configs;
+
+        const logicalName = `${this.name}-storage-${this.input}-bytes`;
+
+        const comparisonOperator = 'GreaterThanOrEqualToThreshold';
+        const anomalyDetectionComparisonOperator = 'GreaterThanUpperThreshold';
+        const namespace = 'ECS/ContainerInsights';
+        const metricName = this.input === 'write' ? 'StorageWriteBytes' : 'StorageReadBytes';
+        const stat = 'Average';
+        const dimensions = { ClusterName: clusterName, ServiceName: serviceName };
+
+        const alarmBuilder = new AlarmBuilder()
+            .name(logicalName, this.extraConfigs?.suffix)
+            .comparisonOperator(comparisonOperator)
+            .evaluationPeriods(this.extraConfigs?.evaluationPeriods)
+            .dataPointsToAlarm(this.extraConfigs?.datapointsToAlarm)
+            .treatMissingData(this.extraConfigs?.treatMissingData)
+            .snsTopicArns(this.extraConfigs?.snsTopicArns)
+            .setParent(parent)
+            .addMetric({
+                id: 'm1',
+                stat,
+                dimensions,
+                metricName,
+                namespace,
+                period: this.extraConfigs?.period,
+                returnData: true,
+            });
+
+        if (this.threshold === 0) {
+            alarmBuilder.hasAnomalyDetection({
+                thresholdMetricId: 'e1',
+                anomalyComparison: anomalyDetectionComparisonOperator,
+                metricToWatchId: 'm1',
+                label: metricName,
+            });
+        }
+
+        return alarmBuilder.build();
+    }
+}
