ca3791ae9047e9e696fb3da19b029d705a0a9607
Rodrigo F Martins <martins.drg@gmail.com>
2023-02-08 20:55:22 -0300

    feat: add utility functions for role creation process

--- src/components/iam/index.ts
+++ src/components/iam/index.ts
@@ -1,1 +1,2 @@
+export { createManagedPolicyArn, createAssumeRolePolicyForUser } from './utils';
 export { RoleArgs, Role } from './role';
---
+++ src/components/iam/utils.ts
@@ -0,0 +1,39 @@
+import * as aws from '@pulumi/aws';
+import * as pulumi from '@pulumi/pulumi';
+
+export function createAssumeRolePolicyForUser(
+    name: pulumi.Input<string>,
+    accountId: pulumi.Input<string>,
+    path?: pulumi.Input<string>
+): aws.types.input.iam.GetPolicyDocumentStatementArgs[] {
+    const policyName = path ? pulumi.interpolate`${path}/${name}` : name;
+
+    return [
+        {
+            actions: ['sts:AssumeRole'],
+            principals: [
+                {
+                    identifiers: [pulumi.interpolate`arn:aws:iam::${accountId}:user/${policyName}`],
+                    type: 'AWS',
+                },
+            ],
+        },
+        {
+            actions: ['sts:TagSession'],
+            principals: [
+                {
+                    identifiers: [pulumi.interpolate`arn:aws:iam::${accountId}:user/${policyName}`],
+                    type: 'AWS',
+                },
+            ],
+        },
+    ];
+}
+
+export function createManagedPolicyArn(
+    name: pulumi.Input<string>,
+    path?: pulumi.Input<string>
+): pulumi.Output<string> {
+    const policyName = path ? pulumi.interpolate`${path}/${name}` : name;
+    return pulumi.interpolate`arn:aws:iam::aws:policy/${policyName}`;
+}
