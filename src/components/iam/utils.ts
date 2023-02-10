66414d54a177abb3baed2fdcb811fb9c8b2d34d0
Rodrigo F Martins <martins.drg@gmail.com>
2023-02-10 11:02:05 -0300

    refactor: fix variable name from policyName to userName

--- src/components/iam/utils.ts
+++ src/components/iam/utils.ts
@@ -6,14 +6,14 @@ export function createAssumeRolePolicyForUser(
     accountId: pulumi.Input<string>,
     path?: pulumi.Input<string>
 ): aws.types.input.iam.GetPolicyDocumentStatementArgs[] {
-    const policyName = path ? pulumi.interpolate`${path}/${name}` : name;
+    const userName = path ? pulumi.interpolate`${path}/${name}` : name;
 
     return [
         {
             actions: ['sts:AssumeRole'],
             principals: [
                 {
-                    identifiers: [pulumi.interpolate`arn:aws:iam::${accountId}:user/${policyName}`],
+                    identifiers: [pulumi.interpolate`arn:aws:iam::${accountId}:user/${userName}`],
                     type: 'AWS',
                 },
             ],
@@ -22,7 +22,7 @@ export function createAssumeRolePolicyForUser(
             actions: ['sts:TagSession'],
             principals: [
                 {
-                    identifiers: [pulumi.interpolate`arn:aws:iam::${accountId}:user/${policyName}`],
+                    identifiers: [pulumi.interpolate`arn:aws:iam::${accountId}:user/${userName}`],
                     type: 'AWS',
                 },
             ],
